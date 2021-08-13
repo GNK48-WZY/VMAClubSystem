f1fa87d25665fbe21abe480d9a3172d2ad71f76d
ddzbxh <duanshuxunhuan@outlook.com>
2021-08-13 18:09:19 +0800

    Update clubs.js

--- Develop/node/dev_modules/database/clubs.js
+++ Develop/node/dev_modules/database/clubs.js
@@ -1,7 +1,7 @@
 /*
     author: SynthesisDu (https://github.com/SynthesisDu)
     software: WebStorm 2021.2, Node.js v14.17.5
-    datetime: 2021.8.13 07:58
+    datetime: 2021.8.13 08:15
     filename: clubs.js
     git_repo: https://github.com/VMAxCoding/VMAClubSystem
  */
