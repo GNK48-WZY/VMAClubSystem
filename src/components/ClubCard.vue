bdad58b10396b2262e34839f4d828cf59cea0306
Blueagle <austinliu@blueagle.top>
2021-10-08 21:01:19 +0800

    t

---
+++ src/components/ClubCard.vue
@@ -0,0 +1,98 @@
+<template>
+  <q-intersection class="q-pa-md card" transition="scale" once>
+    <q-card>
+      <q-img :src="img" />
+
+      <q-card-section>
+        <router-link :to="{ path: `/club/${id.toString()}` }">
+          <q-btn
+            fab
+            push
+            color="primary"
+            icon="launch"
+            class="absolute"
+            style="top: 0; right: 12px; transform: translateY(-50%)"
+          >
+            <q-tooltip
+              transition-show="flip-right"
+              transition-hide="flip-left"
+              anchor="top middle"
+              self="bottom middle"
+            >{{ t("club.viewDetails") }}</q-tooltip>
+          </q-btn>
+        </router-link>
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">{{ name }}</div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            {{ location }}
+          </div>
+        </div>
+      </q-card-section>
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">{{ requirement }}</div>
+        <div class="text-caption text-grey">{{ description }}</div>
+      </q-card-section>
+      <q-separator />
+      <q-card-actions align="right">
+        <q-btn flat color="primary" icon="person_add">{{ t("club.signUp") }}</q-btn>
+        <q-btn flat color="green" icon="add_shopping_cart">
+          {{
+            t("club.joinList")
+          }}
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+  </q-intersection>
+</template>
+<script>
+import { defineComponent } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+export default defineComponent({
+  props: {
+    img: {
+      type: String,
+      default: '',
+    },
+    id: {
+      type: Number,
+      default: 0,
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    location: {
+      type: String,
+      default: '',
+    },
+    requirement: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+  },
+  setup() {
+    const { t } = useI18n();
+    return {
+      t,
+    };
+  },
+});
+</script>
+<style lang="scss" scoped>
+$card-width: 344px;
+.card {
+  @media screen and (min-width: $card-width) {
+    width: $card-width;
+  }
+  @media screen and (max-width: $card-width * 2) {
+    width: 100%;
+  }
+}
+</style>
--- src/pages/ClubCenter.vue
+++ src/pages/ClubCenter.vue
@@ -29,48 +29,14 @@
       </q-intersection>
     </template>
     <template v-slot:item="props">
-      <q-intersection class="q-pa-md qc" transition="scale" once>
-        <q-card>
-          <q-img :src="props.row.imgUrl" />
-
-          <q-card-section>
-            <router-link :to="{ path: `/club/${props.row.id.toString()}` }">
-              <q-btn
-                fab
-                push
-                color="primary"
-                icon="launch"
-                class="absolute"
-                style="top: 0; right: 12px; transform: translateY(-50%);"
-              >
-                <q-tooltip
-                  transition-show="flip-right"
-                  transition-hide="flip-left"
-                  anchor="top middle"
-                  self="bottom middle"
-                >{{ t('club.viewDetails') }}</q-tooltip>
-              </q-btn>
-            </router-link>
-
-            <div class="row no-wrap items-center">
-              <div class="col text-h6 ellipsis">{{ props.row.name }}</div>
-              <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
-                <q-icon name="place" />
-                {{ props.row.location }}
-              </div>
-            </div>
-          </q-card-section>
-          <q-card-section class="q-pt-none">
-            <div class="text-subtitle1">{{ props.row.requirement }}</div>
-            <div class="text-caption text-grey">{{ props.row.description }}</div>
-          </q-card-section>
-          <q-separator />
-          <q-card-actions align="right">
-            <q-btn flat color="primary" icon="person_add">{{ t('club.signUp') }}</q-btn>
-            <q-btn flat color="green" icon="add_shopping_cart">{{ t('club.joinList') }}</q-btn>
-          </q-card-actions>
-        </q-card>
-      </q-intersection>
+      <ClubCard
+        :id="props.row.id"
+        :name="props.row.name"
+        :img="props.row.imgUrl"
+        :requirement="props.row.requirement"
+        :description="props.row.description"
+        :location="props.row.location"
+      />
     </template>
     <template v-slot:no-data="{ message }">
       <div class="text-primary column fit">
@@ -84,11 +50,18 @@
 </template>
 <script>
 import { useStore } from 'vuex';
-import { computed, ref, defineComponent } from 'vue';
+import {
+  computed, ref, defineComponent, defineAsyncComponent,
+} from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useQuasar } from 'quasar';
 
+const ClubCard = defineAsyncComponent(() => import('components/ClubCard'));
+
 export default defineComponent({
+  components: {
+    ClubCard,
+  },
   setup() {
     const store = useStore();
     const { dark } = useQuasar();
@@ -106,21 +79,15 @@ export default defineComponent({
 });
 </script>
 <style lang="scss" scoped>
-$card-width: 344px;
 .qt {
   .search {
     position: fixed;
     z-index: 1;
     right: 0;
     min-width: 50px;
-    opacity: 0.8;
-  }
-  .qc {
-    @media screen and (min-width: $card-width) {
-      width: $card-width;
-    }
-    @media screen and (max-width: $card-width * 2) {
-      width: 100%;
+    opacity: 0.5;
+    :deep(.q-field__control) {
+      backdrop-filter: saturate(180%) blur(20px);
     }
   }
 }
